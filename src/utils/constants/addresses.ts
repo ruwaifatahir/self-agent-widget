bbccf43f8f4247207dcc3e536a7cded6c2b24ff1
ruwaifatahir <66880814+ruwaifatahir@users.noreply.github.com>
2023-11-13 09:09:58 +0500

    update .env.example

--- src/utils/constants/addresses.ts
+++ src/utils/constants/addresses.ts
@@ -3,7 +3,7 @@ import { Address } from "viem";
 export const SELF_TKN_ADDR = process.env.NEXT_PUBLIC_SELF_TOKEN as Address;
 export const SELF_NFT_ADDR = process.env.NEXT_PUBLIC_SELF_NFT_ADDR as Address;
 export const SELF_NFT_ADDON_ADDR = process.env
-  .NEXT_PUBLIC_SELF_NFT_ADDON as Address;
+  .NEXT_PUBLIC_SELF_NFT_ADDON_ADDR as Address;
 
 export const PAY_TKN_ADDRESSES = {
   usdt: process.env.NEXT_PUBLIC_USDT as Address,
